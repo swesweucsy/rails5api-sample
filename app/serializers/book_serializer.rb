5788220c8984ba754cf252d1507549113ff64a28
stella <swesweucsy@gmail.com>
2023-11-27 02:58:40 +0900

    add each book's author and illustrator info into books api

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,3 +1,9 @@
 class BookSerializer < ActiveModel::Serializer
-  attributes :id, :name, :year
+  attributes :id, :name, :year, :book_age
+  belongs_to :author
+  belongs_to :illustrator
+
+  def book_age
+    Date.today.year - (object.year)
+  end
 end
